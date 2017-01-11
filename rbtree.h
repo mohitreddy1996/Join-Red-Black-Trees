327f971ddcf32e6ffb7ca9981a84b292d0b569ae
Ankit Deepak <adadeepak8@gmail.com>
2017-01-11 11:42:01 +0530

    added c++ files

---
+++ rbtree.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+
+
+struct RedBlackTreeNode{
+    int key;
+    int red;
+    struct RedBlackTreeNode *left;
+    struct RedBlackTreeNode *right;
+    struct RedBlackTreeNode *parent;
+};
+
+class RedBlackTree
+{
+
+public:
+void InitialiseSentinel();
+void InitialiseRoot();
+void Insert(int key);
+void Delete(int key);
+void PrintTree();
+
+private:
+const int MIN_INT = -INT_MAX;
+struct RedBlackTreeNode *nil;
+struct RedBlackTreeNode *root;
+void LeftRotate(struct RedBlackTreeNode *RBTreeNode);
+void RightRotate(struct RedBlackTreeNode *RBTreeNode);
+void TreeInsertHelper(struct RedBlackTreeNode *z);
+void InsertNode(struct RedBlackTreeNode *newNode);
+struct RedBlackTreeNode *GetSuccessor(struct RedBlackTreeNode *redBlackTreeNode);
+struct RedBlackTreeNode *GetNode(int key);
+void DeleteFixUp(struct RedBlackTreeNode *x);
+void DeleteNode(struct RedBlackTreeNode *redBlackTreeNode);
+void InOrder(struct RedBlackTreeNode *realRoot);
+};
